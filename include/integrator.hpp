187d2fc17cfd729dccd64d45f6900c0bc6f0fc38
Shaazib Tanvir <syed2302995@st.jmi.ac.in>
2025-10-19 15:20:34 +0530

    feat: added riemann sum

--- include/integrator.hpp
+++ include/integrator.hpp
@@ -3,6 +3,8 @@
 
 #include "types.hpp"
 #include <cstddef>
+#include <cstring>
+#include <iostream>
 #include <random>
 
 template<size_t D>
@@ -27,6 +29,59 @@ f32 monte_carlo(f32 (*f)(f32 args[D]), f32 a[D], f32 b[D], size_t iterations) {
 	}
 
 	return volume * total / iterations;
+}
+
+template<size_t D, size_t M>
+struct RiemannSumHelper {
+	static f32 call(f32 (*f)(f32 args[D]), f32 a[D], f32 b[D], f32 x[D], f32 h[D], f32 volume, size_t iterations, f32 args[M]) {
+		f32 result = 0.0;
+		x[M] = a[M];
+		for (size_t i = 0; i < iterations; i++) {
+			f32 new_args[M+1];
+			std::memcpy(new_args, args, sizeof(f32) * M);
+			new_args[M] = x[M];
+			result += RiemannSumHelper<D, M+1>::call(f, a, b, x, h, volume, iterations, new_args);
+			x[M] += h[M];
+		}
+
+		return result;
+	}
+};
+
+template<size_t D>
+struct RiemannSumHelper<D, D> {
+	static f32 call(f32 (*f)(f32 args[D]), f32 a[D], f32 b[D], f32 x[D], f32 h[D], f32 volume, size_t iterations, f32 args[D]) {
+		return f(args) * volume;
+	}
+};
+
+template<size_t D>
+struct RiemannSumHelper<D, 0> {
+	static f32 call(f32 (*f)(f32 args[D]), f32 a[D], f32 b[D], f32 x[D], f32 h[D], f32 volume, size_t iterations) {
+		f32 result = 0.0;
+		x[0] = a[0];
+		for (size_t i = 0; i < iterations; i++) {
+			f32 new_args[1];
+			new_args[0] = x[0];
+			result += RiemannSumHelper<D, 1>::call(f, a, b, x, h, volume, iterations, new_args);
+			x[0] += h[0];
+		}
+
+		return result;
+	}
+};
+
+template<size_t D>
+f32 riemann_sum(f32 (*f)(f32 args[D]), f32 a[D], f32 b[D], size_t iterations) {
+	f32 h[D];
+	f32 x[D];
+	f32 volume = 1.0f;
+	for (size_t i = 0; i < D; i++) {
+		h[i] = (b[i] - a[i]) / iterations;
+		volume *= h[i];
+	}
+
+	return RiemannSumHelper<D, 0>::call(f, a, b, x, h, volume, iterations);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,45 @@ f32 f(f32 x[1]) {
 	return 0.73*std::sin(2*PI*x[0]+1.12)-0.21*std::sin(4*PI*x[0]+4.57)+0.09*std::sin(6*PI*x[0]+2.03)-0.04*std::sin(8*PI*x[0]+5.68);
 }
 
-int main() {
-	f32 a[1] = {0.0};
-	f32 b[1] = {0.2};
+f32 g(f32 x[2]) {
+	return std::sin(x[0]) * std::cos(x[1]);
+}
 
-	size_t iterations = 1e5;
-	f32 result = monte_carlo<1>(f, a, b, iterations);
-	std::cout << "=====================================================\n";
-	std::cout << "monte carlo\n";
-	std::cout << "iterations = " << iterations << " result = " << result << "\n";
-	std::cout << "=====================================================\n";
+int main() {
+	{
+		f32 a[1] = {0.0};
+		f32 b[1] = {0.2};
+
+		size_t iterations = 1e4;
+		f32 monte_carlo_result = monte_carlo<1>(f, a, b, iterations);
+		std::cout << "=====================================================\n";
+		std::cout << "monte carlo\n";
+		std::cout << "dim = 1 iterations = " << iterations << " result = " << monte_carlo_result << "\n";
+		std::cout << "=====================================================\n";
+
+		f32 riemann_sum_result = riemann_sum<1>(f, a, b, iterations);
+		std::cout << "riemann sum\n";
+		std::cout << "dim = 1 iterations = " << iterations << " result = " << riemann_sum_result << "\n";
+		std::cout << "=====================================================\n";
+	}
+
+	{
+		f32 a[2] = {0.0, 0.0};
+		f32 b[2] = {1.0, 1.0};
+
+		size_t iterations = 1e4;
+		f32 monte_carlo_result = monte_carlo<2>(g, a, b, iterations);
+		std::cout << "=====================================================\n";
+		std::cout << "monte carlo\n";
+		std::cout << "dim = 2 iterations = " << iterations << " result = " << monte_carlo_result << "\n";
+		std::cout << "=====================================================\n";
+
+		size_t i = 1e2;
+		f32 riemann_sum_result = riemann_sum<2>(g, a, b, i);
+		std::cout << "riemann sum\n";
+		std::cout << "dim = 2 iterations = " << iterations << " result = " << riemann_sum_result << "\n";
+		std::cout << "=====================================================\n";
+	}
+
 	return EXIT_SUCCESS;
 }
